b0e344ff40c6f94cbf706d845d626e9218a87c05
rsteward117 <66583624+rsteward117@users.noreply.github.com>
2026-02-17 16:23:18 -0600

    Refactor money converter program with user input

--- moneyConvertor2.cpp
+++ moneyConvertor2.cpp
@@ -1,25 +1,30 @@
-//Randy
-//program to convert dollars to euros and vice versa
-
 #include <iostream>
 using namespace std;
 
 int main() {
-    // declare variables for dollars and euros
-	double dollars = 0, euros = 0;
 
-    euros = .84227; //1 dollar to euro
+    string hello = "Hello, World!";
 
-    // gets the user input
-    cout << "please enter the dollars to convert: " << endl;
-    cin >> dollars;
+    cout << hello << endl;
 
-    // process the conversion
-    dollars = dollars * euros;
-        cout << "Converted amount in euros: " << dollars << endl;
+    // int
+    int number;
+    // this sends output to the console
+    cout << "Enter a number: ";
+    // this gets input from the user
+    cin >> number;
 
-        // go from euros to dollars
-        dollars = euros / .84227;
-        cout << "Converted amount in dollars: " << dollars << endl;
+    if(number <= 0){
+        cout << "Please enter a number greater than 0." << endl;
+    }else{
+        // this outputs the value to the console
+    cout << "You entered: " << number << " this is greater than 0." << endl;
+    }
+
+
+
     return 0;
 }
+
+
+
